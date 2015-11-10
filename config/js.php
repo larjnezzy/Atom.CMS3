6cf0290cc2f2702ffda6fcf792d4b231bb6f3caa
Alan Quandt <alan@auctionvcommerce.com>
2015-11-10 18:18:22 -0500

    Updated link to recent Bootstrap JS

--- config/js.php
+++ config/js.php
@@ -13,7 +13,7 @@
 <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 
 <script>
 	
