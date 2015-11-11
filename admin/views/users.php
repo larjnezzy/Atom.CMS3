bf431b04e5a2315b9584e9c75a18a6fd6a7631fd
Alan Quandt <alan@auctionvcommerce.com>
2015-11-10 19:31:42 -0500

    PHP shortcodes

--- admin/views/users.php
+++ admin/views/users.php
@@ -1,6 +1,6 @@
 <main class="container-fluid">
   
-  <?php if(isset($opened['id'])) { ?>
+  <?if(isset($opened['id'])) { ?>
   	<script>
   		
   		$(document).ready(function() {
@@ -11,14 +11,14 @@
   			
   			myDropzone.on("success", function(file){
   				
-  				$("#avatar").load("ajax/avatar.php?id=<?php echo $opened['id']; ?>");
+  				$("#avatar").load("ajax/avatar.php?id=<?=$opened['id']?>");
   				
   			});
   	
   		});
   	
   	</script>
-  <?php } ?>
+  <?}?>
   
   <h1>User Manager</h1>
   
@@ -32,7 +32,7 @@
   			<i class="fa fa-plus"></i> New User
   		</a>					
   						
-  		<?php 
+  		<?
   		
   			$q = "SELECT * FROM users ORDER BY last ASC";
   			$r = mysqli_query($dbc, $q);
@@ -44,13 +44,13 @@
   					
   			?>
   
-  			<a class="list-group-item <?php selected($list['id'], $opened['id'], 'active'); ?>" href="index.php?page=users&id=<?php echo $list['id']; ?>">
-  				<h4 class="list-group-item-heading"><?php echo $list['fullname_reverse']; ?></h4>
-  				<!--<p class="list-group-item-text"><?php //echo $blurb; ?></p>-->
+  			<a class="list-group-item <?selected($list['id'], $opened['id'], 'active')?>" href="index.php?page=users&id=<?=$list['id']?>">
+  				<h4 class="list-group-item-heading"><?=$list['fullname_reverse']?></h4>
+  				<!--<p class="list-group-item-text"><?php //echo $blurb?></p>-->
   			</a>
   				
   				
-  		<?php } ?>
+  		<?}?>
   		
   		</div>
   		
@@ -58,36 +58,36 @@
   	
   	<div class="col-md-9">
   
-  		<?php if(isset($message)) { echo $message; } ?>
+  		<?if(isset($message)) { echo $message; } ?>
   		
-  		<form action="index.php?page=users&id=<?php echo $opened['id']; ?>" method="post" role="form">
+  		<form action="index.php?page=users&id=<?=$opened['id']?>" method="post" role="form">
   			
   			<div id="avatar">
-  				<?php if($opened['avatar'] != ''){ ?>
+  				<?($opened['avatar'] != ''){?>
   	
-  					<div class="avatar-container" style="background-image: url('../uploads/<?php echo $opened['avatar']; ?>')"></div>
+  					<div class="avatar-container" style="background-image: url('../uploads/<?=$opened['avatar']?>')"></div>
   	
-  				<?php } ?>
+  				<?}?>
   			</div>
   			
   			<div class="form-group">
   				
   				<label for="first">First Name:</label>
-  				<input class="form-control" type="text" name="first" id="first" value="<?php echo $opened['first']; ?>" placeholder="First Name" autocomplete="off">
+  				<input class="form-control" type="text" name="first" id="first" value="<?=$opened['first']?>" placeholder="First Name" autocomplete="off">
   				
   			</div>
   			
   			<div class="form-group">
   				
   				<label for="last">Last Name:</label>
-  				<input class="form-control" type="text" name="last" id="last" value="<?php echo $opened['last']; ?>" placeholder="Last Name" autocomplete="off">
+  				<input class="form-control" type="text" name="last" id="last" value="<?=$opened['last']?>" placeholder="Last Name" autocomplete="off">
   				
   			</div>
   			
   			<div class="form-group">
   				
   				<label for="email">Email Address:</label>
-  				<input class="form-control" type="text" name="email" id="email" value="<?php echo $opened['email']; ?>" placeholder="Email Address" autocomplete="off">
+  				<input class="form-control" type="text" name="email" id="email" value="<?=$opened['email']?>" placeholder="Email Address" autocomplete="off">
   				
   			</div>						
   
@@ -96,8 +96,8 @@
   				<label for="status">Status:</label>
   				<select class="form-control" name="status" id="status">
   					
-  					<option value="0" <?php if(isset($_GET['id'])){ selected('0', $opened['status'], 'selected'); } ?>>Inactive</option>
-  					<option value="1" <?php if(isset($_GET['id'])){ selected('1', $opened['status'], 'selected'); } ?>>Active</option>
+  					<option value="0" <?if(isset($_GET['id'])){ selected('0', $opened['status'], 'selected'); } ?>>Inactive</option>
+  					<option value="1" <?if(isset($_GET['id'])){ selected('1', $opened['status'], 'selected'); } ?>>Active</option>
   										
   				</select>
   				
@@ -119,20 +119,20 @@
   			
   			<button type="submit" class="btn btn-default">Save</button>
   			<input type="hidden" name="submitted" value="1">
-  			<?php if(isset($opened['id'])) { ?>
-  				<input type="hidden" name="id" value="<?php echo $opened['id']; ?>">
-  			<?php } ?>
+  			<?if(isset($opened['id'])) { ?>
+  				<input type="hidden" name="id" value="<?=$opened['id']?>">
+  			<?} ?>
   		</form>
   		
-  		<?php if(isset($opened['id'])) { ?>
+  		<?if(isset($opened['id'])) {?>
   			
-  		<form action="uploads.php?id=<?php echo $opened['id']; ?>" class="dropzone" id="avatar-dropzone">
+  		<form action="uploads.php?id=<?=$opened['id']?>" class="dropzone" id="avatar-dropzone">
   			
   			<input type="file" name="file">
   			
   		</form>
   		
-  		<?php } ?>
+  		<?}?>
   		
   	</div>
   		
