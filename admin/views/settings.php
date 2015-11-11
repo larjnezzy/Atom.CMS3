cbcbdb1ab45beb39e7ea6ba96780717f7f3054af
Alan Quandt <alan@auctionvcommerce.com>
2015-11-10 19:23:21 -0500

    Support for PDO

--- admin/views/settings.php
+++ admin/views/settings.php
@@ -7,39 +7,39 @@
 		<?php if(isset($message)) { echo $message; } ?>
 			
 		<?php 
-		
-			$q = "SELECT * FROM settings ORDER BY id ASC";
-			$r = mysqli_query($dbc, $q);
-		
-			while($opened = mysqli_fetch_assoc($r)) { ?>
 
-				<form class="form-inline" action="index.php?page=settings&id=<?php echo $opened['id']; ?>" method="post" role="form">
+      $stmt = $dbc->query("SELECT * FROM settings ORDER BY id ASC");
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+          
+      while($data = $stmt->fetch()){?>
+
+				<form class="form-inline" action="index.php?page=settings&id=<?php echo $data['id']; ?>" method="post" role="form">
 
 					<div class="form-group">
 						
 						<label class="sr-only" for="id">ID:</label>
-						<input class="form-control" data-id="<?php echo $opened['id']; ?>" data-label="Setting ID" data-db="settings-id" type="text" name="id" id="id" value="<?php echo $opened['id']; ?>" placeholder="id-name" autocomplete="off" disabled>
+						<input class="form-control" data-id="<?php echo $data['id']; ?>" data-label="Setting ID" data-db="settings-id" type="text" name="id" id="id" value="<?php echo $data['id']; ?>" placeholder="id-name" autocomplete="off" disabled>
 						
 					</div>
 					
 					<div class="form-group">
 						
 						<label class="sr-only" for="label">Label:</label>
-						<input class="form-control blur-save" data-id="<?php echo $opened['id']; ?>" data-label="Setting Label" data-db="settings-label" type="text" name="label" id="label" value="<?php echo $opened['label']; ?>" placeholder="Label" autocomplete="off">
+						<input class="form-control blur-save" data-id="<?php echo $data['id']; ?>" data-label="Setting Label" data-db="settings-label" type="text" name="label" id="label" value="<?php echo $data['label']; ?>" placeholder="Label" autocomplete="off">
 						
 					</div>
 					
 					<div class="form-group">
 						
 						<label class="sr-only" for="value">Value:</label>
-						<input class="form-control blur-save" data-id="<?php echo $opened['id']; ?>" data-label="Setting Value" data-db="settings-value" type="text" name="value" id="value" value="<?php echo $opened['value']; ?>" placeholder="Value" autocomplete="off">
+						<input class="form-control blur-save" data-id="<?php echo $data['id']; ?>" data-label="Setting Value" data-db="settings-value" type="text" name="value" id="value" value="<?php echo $data['value']; ?>" placeholder="Value" autocomplete="off">
 						
 					</div>						
 		
 					<button type="submit" class="btn btn-default">Save</button>
 					<input type="hidden" name="submitted" value="1">
 					
-					<input type="hidden" name="openedid" value="<?php echo $opened['id']; ?>">
+					<input type="hidden" name="openedid" value="<?php echo $data['id']; ?>">
 					
 				</form>
 
