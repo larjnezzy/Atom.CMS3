759fac85b550b78f016bb7c46a0242ceab08dfaa
Alan Quandt <alan@auctionvcommerce.com>
2015-11-10 19:25:13 -0500

    Container

--- admin/views/pages.php
+++ admin/views/pages.php
@@ -1,130 +1,133 @@
-<h1>Page Manager</h1>
+<main class="container-fluid">
 
-
-<div class="row">
-	
-	<div class="col-md-3">
-
-		<div class="list-group">
-			
-		<a class="list-group-item" href="?page=pages">
-			<i class="fa fa-plus"></i> New Page
-		</a>					
-						
-		<? 
-
-      $stmt = $dbc->query("SELECT * FROM posts WHERE type = 1 ORDER BY title ASC");
-      $stmt->setFetchMode(PDO::FETCH_ASSOC);
-      
-      while($data= $stmt->fetch()){
-			
-				$blurb = substr(strip_tags($data['body']), 0, 160);
-					
-			?>
-
-			<div id="page_<?=$data['id']?>" class="list-group-item <?selected($data['id'], $opened['id'], 'active')?>">
-				<h4 class="list-group-item-heading"><?=$data['title']?>
-				<span class="pull-right">
-					<a href="#" id="del_<?=$data['id']?>" class="btn btn-danger btn-delete"><i class="fa fa-trash-o"></i></a>
-					<a href="index.php?page=pages&id=<?=$data['id']?>" class="btn btn-default"><i class="fa fa-chevron-right"></i></a>
-				</span>
-				
-				</h4>
-				<p class="list-group-item-text"><?=$blurb?></p>
-			</div>
-				
-				
-		<?}?>
-		
-		</div>
-		
-	</div>
-	
-	<div class="col-md-9">
-
-		<?if(isset($message)) { echo $message; }?>
-		
-		<form action="index.php?page=pages&id=<?=$opened['id']?>" method="post" role="form">
-			
-			
-			<div class="form-group">
-				
-				<label for="title">Title:</label>
-				<input class="form-control" type="text" name="title" id="title" value="<?=$opened['title']?>" placeholder="Page Title">
-				
-			</div>
-
-			<div class="form-group">
-				
-				<label for="user">User:</label>
-				<select class="form-control" name="user" id="user">
-					
-					<option value="0">No user</option>
-					
-					 <?
-					
-						$q = "SELECT id FROM users ORDER BY first ASC";
-						$r = mysqli_query($dbc, $q);
-						
-						while($user_list = mysqli_fetch_assoc($r)) { 
-						
-							$user_data = data_user($dbc, $user_list['id']);
-								
-						?>
-					
-							<option value="<?=$user_data['id']?>" 
-								<? 
-									if(isset($_GET['id'])){
-										selected($user_data['id'], $opened['user'], 'selected');
-									} else {												
-										selected($user_data['id'], $user['id'], 'selected');
-									}
-									
-								
-								?>><?=$user_data['fullname']?></option>
-					
-						<?}?>
-					
-				</select>
-				
-			</div>
-
-			<div class="form-group">
-				
-				<label for="slug">Slug:</label>
-				<input class="form-control" type="text" name="slug" id="slug" value="<?=$opened['slug']?>" placeholder="Page Slug">
-				
-			</div>
-
-			<div class="form-group">
-				
-				<label for="label">Label:</label>
-				<input class="form-control" type="text" name="label" id="label" value="<?=$opened['label']?>" placeholder="Page Label">
-				
-			</div>
-			
-			<div class="form-group">
-				
-				<label for="header">Header:</label>
-				<input class="form-control" type="text" name="header" id="header" value="<?=$opened['header']?>" placeholder="Page Header">
-				
-			</div>										
-
-			<div class="form-group">
-				
-				<label for="body">Body:</label>
-				<textarea class="form-control editor" name="body" id="body" rows="8" placeholder="Page Body"><?=$opened['body']?></textarea>
-				
-			</div>
-			
-			<button type="submit" class="btn btn-default">Save</button>
-			<input type="hidden" name="submitted" value="1">
-			<?if(isset($opened['id'])) {?>
-				<input type="hidden" name="id" value="<?=$opened['id']?>">
-			<?}?>
-			
-		</form>
-		
-	</div>
-		
-</div>
+  <h1>Page Manager</h1>
+  
+  
+  <div class="row">
+  	
+  	<div class="col-md-3">
+  
+  		<div class="list-group">
+  			
+  		<a class="list-group-item" href="?page=pages">
+  			<i class="fa fa-plus"></i> New Page
+  		</a>					
+  						
+  		<? 
+  
+        $stmt = $dbc->query("SELECT * FROM posts WHERE type = 1 ORDER BY title ASC");
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        
+        while($data= $stmt->fetch()){
+  			
+  				$blurb = substr(strip_tags($data['body']), 0, 160);
+  					
+  			?>
+  
+  			<div id="page_<?=$data['id']?>" class="list-group-item <?selected($data['id'], $opened['id'], 'active')?>">
+  				<h4 class="list-group-item-heading"><?=$data['title']?>
+  				<span class="pull-right">
+  					<a href="#" id="del_<?=$data['id']?>" class="btn btn-danger btn-delete"><i class="fa fa-trash-o"></i></a>
+  					<a href="index.php?page=pages&id=<?=$data['id']?>" class="btn btn-default"><i class="fa fa-chevron-right"></i></a>
+  				</span>
+  				
+  				</h4>
+  				<p class="list-group-item-text"><?=$blurb?></p>
+  			</div>
+  				
+  				
+  		<?}?>
+  		
+  		</div>
+  		
+  	</div>
+  	
+  	<div class="col-md-9">
+  
+  		<?if(isset($message)) { echo $message; }?>
+  		
+  		<form action="index.php?page=pages&id=<?=$opened['id']?>" method="post" role="form">
+  			
+  			
+  			<div class="form-group">
+  				
+  				<label for="title">Title:</label>
+  				<input class="form-control" type="text" name="title" id="title" value="<?=$opened['title']?>" placeholder="Page Title">
+  				
+  			</div>
+  
+  			<div class="form-group">
+  				
+  				<label for="user">User:</label>
+  				<select class="form-control" name="user" id="user">
+  					
+  					<option value="0">No user</option>
+  					
+  					 <?
+  					
+  						$q = "SELECT id FROM users ORDER BY first ASC";
+  						$r = mysqli_query($dbc, $q);
+  						
+  						while($user_list = mysqli_fetch_assoc($r)) { 
+  						
+  							$user_data = data_user($dbc, $user_list['id']);
+  								
+  						?>
+  					
+  							<option value="<?=$user_data['id']?>" 
+  								<? 
+  									if(isset($_GET['id'])){
+  										selected($user_data['id'], $opened['user'], 'selected');
+  									} else {												
+  										selected($user_data['id'], $user['id'], 'selected');
+  									}
+  									
+  								
+  								?>><?=$user_data['fullname']?></option>
+  					
+  						<?}?>
+  					
+  				</select>
+  				
+  			</div>
+  
+  			<div class="form-group">
+  				
+  				<label for="slug">Slug:</label>
+  				<input class="form-control" type="text" name="slug" id="slug" value="<?=$opened['slug']?>" placeholder="Page Slug">
+  				
+  			</div>
+  
+  			<div class="form-group">
+  				
+  				<label for="label">Label:</label>
+  				<input class="form-control" type="text" name="label" id="label" value="<?=$opened['label']?>" placeholder="Page Label">
+  				
+  			</div>
+  			
+  			<div class="form-group">
+  				
+  				<label for="header">Header:</label>
+  				<input class="form-control" type="text" name="header" id="header" value="<?=$opened['header']?>" placeholder="Page Header">
+  				
+  			</div>										
+  
+  			<div class="form-group">
+  				
+  				<label for="body">Body:</label>
+  				<textarea class="form-control editor" name="body" id="body" rows="8" placeholder="Page Body"><?=$opened['body']?></textarea>
+  				
+  			</div>
+  			
+  			<button type="submit" class="btn btn-default">Save</button>
+  			<input type="hidden" name="submitted" value="1">
+  			<?if(isset($opened['id'])) {?>
+  				<input type="hidden" name="id" value="<?=$opened['id']?>">
+  			<?}?>
+  			
+  		</form>
+  		
+  	</div>
+  		
+  </div>
+</main>
--- admin/views/settings.php
+++ admin/views/settings.php
@@ -1,50 +1,51 @@
-<h1>Site Settings</h1>
+<main class="container-fluid">
 
-<div class="row">
-	
-	<div class="col-md-12">
-
-		<?if(isset($message)) { echo $message; }?>
-			
-		<?
-
-      $stmt = $dbc->query("SELECT * FROM settings ORDER BY id ASC");
-      $stmt->setFetchMode(PDO::FETCH_ASSOC);
-          
-      while($data = $stmt->fetch()){?>
-
-				<form class="form-inline" action="index.php?page=settings&id=<?=$data['id']?>" method="post" role="form">
-
-					<div class="form-group">
-						
-						<label class="sr-only" for="id">ID:</label>
-						<input class="form-control" data-id="<?=$data['id']?>" data-label="Setting ID" data-db="settings-id" type="text" name="id" id="id" value="<?=$data['id']?>" placeholder="id-name" autocomplete="off" disabled>
-						
-					</div>
-					
-					<div class="form-group">
-						
-						<label class="sr-only" for="label">Label:</label>
-						<input class="form-control blur-save" data-id="<?=$data['id']?>" data-label="Setting Label" data-db="settings-label" type="text" name="label" id="label" value="<?=$data['label']?>" placeholder="Label" autocomplete="off">
-						
-					</div>
-					
-					<div class="form-group">
-						
-						<label class="sr-only" for="value">Value:</label>
-						<input class="form-control blur-save" data-id="<?=$data['id']?>" data-label="Setting Value" data-db="settings-value" type="text" name="value" id="value" value="<?=$data['value']?>" placeholder="Value" autocomplete="off">
-						
-					</div>						
-		
-					<button type="submit" class="btn btn-default">Save</button>
-					<input type="hidden" name="submitted" value="1">
-					
-					<input type="hidden" name="openedid" value="<?=$data['id']?>">
-					
-				</form>
-
-		<?}?>
-
-	</div>
-		
-</div>
+  <h1>Site Settings</h1>
+  
+  <div class="row">
+  	
+  	<div class="col-md-12">
+  
+  		<?if(isset($message)) { echo $message; }
+      
+        $stmt = $dbc->query("SELECT * FROM settings ORDER BY id ASC");
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            
+        while($data = $stmt->fetch()){?>
+  
+  				<form class="form-inline" action="index.php?page=settings&id=<?=$data['id']?>" method="post" role="form">
+  
+  					<div class="form-group">
+  						
+  						<label class="sr-only" for="id">ID:</label>
+  						<input class="form-control" data-id="<?=$data['id']?>" data-label="Setting ID" data-db="settings-id" type="text" name="id" id="id" value="<?=$data['id']?>" placeholder="id-name" autocomplete="off" disabled>
+  						
+  					</div>
+  					
+  					<div class="form-group">
+  						
+  						<label class="sr-only" for="label">Label:</label>
+  						<input class="form-control blur-save" data-id="<?=$data['id']?>" data-label="Setting Label" data-db="settings-label" type="text" name="label" id="label" value="<?=$data['label']?>" placeholder="Label" autocomplete="off">
+  						
+  					</div>
+  					
+  					<div class="form-group">
+  						
+  						<label class="sr-only" for="value">Value:</label>
+  						<input class="form-control blur-save" data-id="<?=$data['id']?>" data-label="Setting Value" data-db="settings-value" type="text" name="value" id="value" value="<?=$data['value']?>" placeholder="Value" autocomplete="off">
+  						
+  					</div>						
+  		
+  					<button type="submit" class="btn btn-default">Save</button>
+  					<input type="hidden" name="submitted" value="1">
+  					
+  					<input type="hidden" name="openedid" value="<?=$data['id']?>">
+  					
+  				</form>
+  
+  		<?}?>
+  
+  	</div>
+  		
+  </div>
+</main>
